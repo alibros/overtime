a3b1de3d33a661c3dd59a4ccd0686bafb137774b
Ali Bros <eilya.bros@gmail.com>
2025-02-05 17:15:24 +0000

    Initial Commit

---
+++ OverTime/OverTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverTimeApp.swift
+//  OverTime
+//
+//  Created by Ali Bros on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct OverTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
